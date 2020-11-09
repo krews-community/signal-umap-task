6d519dd075339433566e7173abb00dc485a89c12
hpratt <hpratt111@live.com>
2020-11-08 20:57:44 -0500

    add numpy

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 set -e
 
 python3 -m pip install --upgrade setuptools wheel
-python3 -m pip install --user ujson umap-learn sklearn
+python3 -m pip install --user numpy ujson umap-learn sklearn
 
 # cd to project root directory
 cd "$(dirname "$(dirname "$0")")"
