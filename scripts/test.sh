4f77d7b7c95ce8b3e081b19573e97d0544863159
hpratt <hpratt111@live.com>
2020-11-08 20:50:55 -0500

    add UMAP back, add wheel

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-python3 -m pip install --upgrade setuptools
-python3 -m pip install --user ujson sklearn
+python3 -m pip install --upgrade setuptools wheel
+python3 -m pip install --user ujson umap-learn sklearn
 
 # cd to project root directory
 cd "$(dirname "$(dirname "$0")")"
